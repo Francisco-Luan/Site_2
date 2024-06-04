5769c15cae808b61bf4ce6f7e8d61b519fac30ff
Luan-Roddrigues <luanrodriguespf9@gmail.com>
2024-06-04 17:44:32 -0300

    atualização

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector('.menu');
 const navMenu = document.querySelectorAll('.nav-menu');
 
-menu.addEventListener('click', () -> {
-    menu.classList.toggle('ativo');
+menu.addEventListener('click', (){
+    menu.classListtoggle('ativo');
     navMenu.classList.toggle('ativo');
 })
